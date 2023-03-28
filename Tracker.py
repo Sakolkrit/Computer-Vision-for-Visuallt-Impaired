656cc1a7c492a08630fbd5d3d0b55011cf89ecc3
Nuttawat C <nuttawac@scg.com>
2023-03-28 19:47:04 +0700

    Make tracker more real-time by utilizing threading and Queue

--- Tracker.py
+++ Tracker.py
@@ -6,6 +6,8 @@ import multiprocessing
 import time
 import os
 
+from video_streaming import FileVideoStream
+
 
 # Distance constants
 KNOWN_DISTANCE_S = 0.99 # meters
@@ -50,8 +52,9 @@ net.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
 
 # object detector funciton /method
 def object_detector(image):
-    
+    start_time = time.time()
     classes, scores, boxes = net.detect(image, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
+    print("model prediction: ", time.time() - start_time)
     # creating empty list to add objects data
     data_list = []
 
@@ -59,6 +62,7 @@ def object_detector(image):
     global i,j,w,h
     
     for (classid, score, box) in zip(classes, scores, boxes):
+        
         #print(classid, score)
         i, j, w, h = box
 
@@ -67,22 +71,15 @@ def object_detector(image):
 
         label = "%s : %f" % (class_names[classid], score)
 
-        # draw rectangle on and label on object
-        #cv2.rectangle(image, box, color, 2)
-        #cv2.putText(image, label, (box[0], box[1] - 14), FONTS, 0.5, color, 2)
-
-
-
-
         # getting the data
         # 1: class name  2: object width in pixels, 3: position where have to draw text(distance)
         if classid == 1:  # stop sign class id
             data_list.append(["sign", box[2], (box[0], box[1] - 2)])
 
-            cv2.rectangle(image, box, color, 2)
-            cv2.putText(image, label, (box[0], box[1] - 14), FONTS, 0.5, color, 2)
+            #cv2.rectangle(image, box, color, 2)
+            #cv2.putText(image, label, (box[0], box[1] - 14), FONTS, 0.5, color, 2)
 
-        if classid == 2:
+        elif classid == 2:
             data_list.append(["tree", box[2], (box[0], box[1] - 2)])
 
             cv2.rectangle(image, box, color, 2)
@@ -97,7 +94,7 @@ def object_detector(image):
 
         # if you want to include more classes then you have to simply add more [elif] statements here
         # returning list containing the object data.
-
+        print("object detection: ", time.time()-start_time)
     return data_list
 
 
@@ -106,7 +103,7 @@ def object_detector(image):
 def focal_length_finder(measured_distance, real_width, width_in_rf):
     focal_length = (width_in_rf * measured_distance) / real_width
 
-    return focal_length / IMG_7142
+    return focal_length / 7
 
 
 
@@ -116,51 +113,24 @@ def distance_finder(focal_length, real_object_width, width_in_frmae):
     distance = (real_object_width * focal_length) / width_in_frmae
     return distance
 
-
-
-
-
-cap = cv2.VideoCapture(-1)
-
-
-
-
+#cap = cv2.VideoCapture(-1)
 
 # reading the reference image from dir
 ref_sign = cv2.imread('IMG_7142.jpg')
 ref_tree = cv2.imread('tree.JPG')
 ref_wires = cv2.imread('wires_1.JPG')
 
-
-
-
 sign_data = object_detector(ref_sign)
 sign_width_in_rf = sign_data[0][1]
-
-
 tree_data = object_detector(ref_tree)
 tree_width_in_rf = tree_data[0][1]
-
 wires_data = object_detector(ref_wires)
 wires_width_in_rf = wires_data[0][1]
 
-
-
-
-
-
-
-
-
 # finding focal length
 focal_tree = focal_length_finder(KNOWN_DISTANCE_T, TREE_WIDTH, tree_width_in_rf)
-
 focal_wires = focal_length_finder(KNOWN_DISTANCE_W, WIRES_WIDTH, wires_width_in_rf)
-
 focal_sign = focal_length_finder(KNOWN_DISTANCE_S, SIGN_WIDTH, sign_width_in_rf)
-
-
-
 
 
 notif_count = 0
@@ -174,31 +144,33 @@ def sound_player(filename = ""):
     fpath = os.path.join(sound_root, fname)
     playsound(fpath)
 
+frame_queue = FileVideoStream(-1).start()
 
 
 while True:
-    ret, frame = cap.read()
+    #ret, frame = cap.read()
+    frame = frame_queue.read()
+    
     data = object_detector(frame)
-
-
-
+    #cv2.waitKey(50)
     if len(data) > 0:
         consecutive_safe_counter = 0
         for d in data:
             objectName = d[0]
 
             if objectName == 'sign':
+                
                 x, y = d[2]  # position where to draw text, dist
                 distance = distance_finder(focal_sign, SIGN_WIDTH, d[1])
 
-                cv2.rectangle(frame, (x, y - 3), (x + 150, y + 23), BLACK, -1)
-                cv2.putText(frame, f'Dis: {round(distance, 2)} m', (x + 5, y + 13), FONTS, 0.48, GREEN, 2)
+                #cv2.rectangle(frame, (x, y - 3), (x + 150, y + 23), BLACK, -1)
+                #cv2.putText(frame, f'Dis: {round(distance, 2)} m', (x + 5, y + 13), FONTS, 0.48, GREEN, 2)
 
                 #img capped from webcam dimensions: (720, 1280, 3)
                 if distance < 15:
                     #if success:
                         #if notif_count == 0:
-                    print("i: {}, w: {}, (i+w)*2: {}".format(i, w, (i+w)*2))
+                    #print("i: {}, w: {}, (i+w)*2: {}".format(i, w, (i+w)*2))
                     if (i+w)*2 in range(0,640): #left-half region
                         # print('Move right')
                         # playsound('Move_right.mp3', False)
@@ -222,118 +194,22 @@ while True:
                         #time.sleep(w_time)  # delay for 3 secs
                         notif_count += 1
 
-            if objectName == 'tree':
-                x, y = d[2]  # position where to draw text, dist
-                distance = distance_finder(focal_tree, TREE_WIDTH, d[1])
-
-                cv2.rectangle(frame, (x, y - 3), (x + 150, y + 23), BLACK, -1)
-                cv2.putText(frame, f'Dis: {round(distance, 2)} m', (x + 5, y + 13), FONTS, 0.48, GREEN, 2)
-
-                #img capped from webcam dimensions: (720, 1280, 3)
-                if distance < 15:
-                    #if success:
-                        #if notif_count == 0:
-                    if (i+w) in range(0,640): #left-half region
-                        # print('Move right')
-                        # playsound('Move_left.mp3', False)
-                        # ------ new way to play sound
-                        print('Beware your left')
-                        if notif_count%30 == 0:
-                            notif_count = 0
-                            sound_player("move_left")
-                        # ------end  new way to play sound
-                        #time.sleep(w_time)  # delay for 3 secs
-                        notif_count += 1
-
-                    elif (i+w) in range(641,1280): #right-half region
-                        # print('Move left')
-                        # playsound('Move_left.mp3')
-
-                        # ------ new way to play sound
-                        print('Beware your right')
-                        if notif_count%30 == 0:
-                            notif_count = 0
-                            sound_player("move_right")
-                        # ------end  new way to play sound
-
-                        #time.sleep(w_time)  # delay for 3 secs
-                        notif_count += 1
-            elif objectName == 'wires':
-                x, y = d[2]  # position where to draw text, dist
-                distance = distance_finder(focal_wires, WIRES_WIDTH, d[1])
-
-                cv2.rectangle(frame, (x, y - 3), (x + 150, y + 23), BLACK, -1)
-                cv2.putText(frame, f'Dis: {round(distance, 2)} m', (x + 5, y + 13), FONTS, 0.48, GREEN, 2)
-
-                # img capped from webcam dimensions: (720, 1280, 3)
-                if distance < 15:
-                    # if success:
-                    # if notif_count == 0:
-                    if (i + w) in range(0, 640):  # left-half region
-                        # print('Move right')
-                        # playsound('Move_right.mp3')
-                        # ------ new way to play sound
-                        print('Beware your left')
-                        if notif_count%30 == 0:
-                            notif_count = 0
-                            sound_player("move_left")
-                        # ------end  new way to play sound
-
-                        # time.sleep(w_time)  # delay for 3 secs
-                        notif_count += 1
-
-                    elif (i + w) in range(641, 1280):  # right-half region
-                        # print('Move left')
-                        # playsound('Move_left.mp3')
-
-                        # ------ new way to play sound
-                        print('Beware your right')
-                        if notif_count%30 == 0:
-                            notif_count = 0
-                            sound_player("move_right")
-                        # ------end  new way to play sound
-
-                        # time.sleep(w_time)  # delay for 3 secs
-                        notif_count += 1
-
     else:
-        consecutive_safe_counter += 1# ----- new adding
+        consecutive_safe_counter += 1
         if notif_count > 0:
-            if consecutive_safe_counter >= 10:# ----- new adding
-                consecutive_safe_counter = 0# ----- new adding
-                # play safe sound
-                # print('Safe')
-                # playsound('Safe.mp3')
-                # ------ new way to play sound
+            if consecutive_safe_counter >= 1:
+                consecutive_safe_counter = 0
                 print('Safe')
                 sound_player("safe")
-                # ------end  new way to play sound
-                # print(box)
+                
                 notif_count = 0
-                # print(i + w)
 
-
-
-
-
-        # elif d[0] == 'cell phone':
-        #     distance = distance_finder(focal_mobile, MOBILE_WIDTH, d[1])
-        #     x, y = d[2]
-
-
-
-
-
-        print(box)
-        print(i+w)
-
-
-
-
-    cv2.imshow('frame', frame)
+    #cv2.imshow('frame', frame)
+    
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
+    
 
 cv2.destroyAllWindows()
 cap.release()
